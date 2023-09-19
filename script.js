f88365d16a7e07df3470274c086aa997eff9fc69
Jay Gupta <66860950+JayGupta797@users.noreply.github.com>
2023-09-18 21:15:38 -0700

    Initial Commit
    bare-bones website with html, css and javascript.
    
    TODO: make this responsive for mobile users.

---
+++ script.js
@@ -0,0 +1,66 @@
+
+/* Java Script to perform main page toggle functionality */
+let data = localStorage.getItem('theme');
+
+if (data == null) {
+  localStorage.setItem('theme', 'light');
+
+  // Adjust prism-theme
+  element = document.getElementById("prism-theme")
+  if (element) {
+    element.setAttribute('integrity', 'sha512-/mZ1FHPkg6EKcxo0fKXF51ak6Cr2ocgDi5ytaTBjsQZIH/RNs6GF6+oId/vPe3eJB836T36nXwVh/WBl/cWT4w==')
+    element.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/themes/prism.min.css')
+  }
+}
+
+if (data == 'light') {
+  document.getElementById("moon").classList.remove('night');
+  document.body.classList.remove("dark-theme");
+
+  // Adjust prism-theme
+  element = document.getElementById("prism-theme")
+  if (element) {
+    element.setAttribute('integrity', 'sha512-/mZ1FHPkg6EKcxo0fKXF51ak6Cr2ocgDi5ytaTBjsQZIH/RNs6GF6+oId/vPe3eJB836T36nXwVh/WBl/cWT4w==')
+    element.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/themes/prism.min.css')
+  }
+}
+
+else if (data == 'dark') {
+  document.getElementById("moon").classList.add('night');
+  document.body.classList.add("dark-theme");
+
+  // Adjust prism-theme
+  element = document.getElementById("prism-theme")
+
+  if (element) {
+    element.setAttribute('integrity', 'sha512-5HvW0a7ihK3ro2KhwEksDHXgIezsTeZybZDIn8d8Y015Ny+t7QWSIjnlCTjFzlK7Klb604HLGjsNqU/i5mJLjQ==')
+    element.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/themes/prism-okaidia.min.css')
+  }
+}
+
+function toggleDark() {
+  moon = document.getElementById("moon")
+  moon.classList.toggle('night');
+
+  body = document.body;
+  body.classList.toggle("dark-theme");
+
+  if (body.classList.contains("dark-theme")) {
+    localStorage.setItem('theme', 'dark');
+
+    element = document.getElementById("prism-theme")
+    if (element) {
+      element.setAttribute('integrity', 'sha512-5HvW0a7ihK3ro2KhwEksDHXgIezsTeZybZDIn8d8Y015Ny+t7QWSIjnlCTjFzlK7Klb604HLGjsNqU/i5mJLjQ==')
+      element.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/themes/prism-okaidia.min.css')
+    }
+  }
+
+  else {
+    localStorage.setItem('theme', 'light');
+    element = document.getElementById("prism-theme")
+    if (element) {
+      element.setAttribute('integrity', 'sha512-/mZ1FHPkg6EKcxo0fKXF51ak6Cr2ocgDi5ytaTBjsQZIH/RNs6GF6+oId/vPe3eJB836T36nXwVh/WBl/cWT4w==')
+      element.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/themes/prism.min.css')
+    }
+  }
+}
---
+++ table.js
@@ -0,0 +1,44 @@
+
+// Function to populate the table from headers
+function populateTable() {
+    const headerContainer = document.getElementById("headerContainer");
+    const tableBody = document.getElementById("tableBody");
+
+    // Get all the headers and subheaders
+    const headers = Array.from(headerContainer.querySelectorAll("h, h2"));
+
+    // Iterate through headers and populate the table
+    headers.forEach(header => {
+
+        // add an id to each header
+        header.setAttribute("id", header.textContent.replaceAll(" ", "-"));
+
+        // create relevant elements and class attributes
+        const row = document.createElement("tr");
+        const cell = document.createElement("td");
+        const anchor = document.createElement("a");
+
+        if (header.tagName === "H") {
+            cell.setAttribute("class", "hd");
+            // cell.classList.add("hd");
+        }
+
+        if (header.tagName === "H2") {
+            cell.setAttribute("class", "sb");
+            // cell.classList.add("sb");
+        }
+
+        // setup the anchor
+        anchor.textContent = header.textContent;
+        anchor.href = "#" + header.textContent.replaceAll(" ", "-")
+        anchor.target = "_self"
+
+        // append children
+        cell.appendChild(anchor);
+        row.appendChild(cell);
+        tableBody.appendChild(row);
+    });
+}
+
+// Call the populateTable function
+populateTable();
