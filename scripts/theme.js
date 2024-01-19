bd91c5d20de4d1f98677125bf99f4a2cc1fb44d7
Jay Gupta <66860950+JayGupta797@users.noreply.github.com>
2024-01-19 01:04:28 -0800

    See Description
    There are a lot of changes here. Most notably, I updated the svgs to be read in from assets. This helps keep the relevant html portable and readable. It also helps me abstract away the issue with viewports and such into easier to understand measures (i.e. width and height). I also removed the width and height attributes from the svgs to be web-safe with safari. Before, spacing was an issue. I also added a reading time attribute to the post section.
    
    Also started linear algebra blog and fixed minor typos in the previous blogs

--- scripts/theme.js
+++ scripts/theme.js
@@ -8,8 +8,8 @@ if (data == null) {
   // Adjust prism-theme
   element = document.getElementById("prism-theme")
   if (element) {
-    element.setAttribute('integrity', 'sha512-/mZ1FHPkg6EKcxo0fKXF51ak6Cr2ocgDi5ytaTBjsQZIH/RNs6GF6+oId/vPe3eJB836T36nXwVh/WBl/cWT4w==')
-    element.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/themes/prism.min.css')
+    element.setAttribute('integrity', 'sha512-/mZ1FHPkg6EKcxo0fKXF51ak6Cr2ocgDi5ytaTBjsQZIH/RNs6GF6+oId/vPe3eJB836T36nXwVh/WBl/cWT4w==');
+    element.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/themes/prism.min.css');
   }
 }
 
@@ -20,8 +20,8 @@ if (data == 'light') {
   // Adjust prism-theme
   element = document.getElementById("prism-theme")
   if (element) {
-    element.setAttribute('integrity', 'sha512-/mZ1FHPkg6EKcxo0fKXF51ak6Cr2ocgDi5ytaTBjsQZIH/RNs6GF6+oId/vPe3eJB836T36nXwVh/WBl/cWT4w==')
-    element.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/themes/prism.min.css')
+    element.setAttribute('integrity', 'sha512-/mZ1FHPkg6EKcxo0fKXF51ak6Cr2ocgDi5ytaTBjsQZIH/RNs6GF6+oId/vPe3eJB836T36nXwVh/WBl/cWT4w==');
+    element.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/themes/prism.min.css');
   }
 }
 
@@ -33,13 +33,14 @@ else if (data == 'dark') {
   element = document.getElementById("prism-theme")
 
   if (element) {
-    element.setAttribute('integrity', 'sha512-5HvW0a7ihK3ro2KhwEksDHXgIezsTeZybZDIn8d8Y015Ny+t7QWSIjnlCTjFzlK7Klb604HLGjsNqU/i5mJLjQ==')
-    element.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/themes/prism-okaidia.min.css')
+    element.setAttribute('integrity', 'sha512-5HvW0a7ihK3ro2KhwEksDHXgIezsTeZybZDIn8d8Y015Ny+t7QWSIjnlCTjFzlK7Klb604HLGjsNqU/i5mJLjQ==');
+    element.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/themes/prism-okaidia.min.css');
   }
 }
 
 function toggleDark() {
-  moon = document.getElementById("moon")
+
+  moon = document.getElementById("moon");
   moon.classList.toggle('night');
 
   body = document.body;
@@ -48,19 +49,19 @@ function toggleDark() {
   if (body.classList.contains("dark-theme")) {
     localStorage.setItem('theme', 'dark');
 
-    element = document.getElementById("prism-theme")
+    element = document.getElementById("prism-theme");
     if (element) {
-      element.setAttribute('integrity', 'sha512-5HvW0a7ihK3ro2KhwEksDHXgIezsTeZybZDIn8d8Y015Ny+t7QWSIjnlCTjFzlK7Klb604HLGjsNqU/i5mJLjQ==')
-      element.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/themes/prism-okaidia.min.css')
+      element.setAttribute('integrity', 'sha512-5HvW0a7ihK3ro2KhwEksDHXgIezsTeZybZDIn8d8Y015Ny+t7QWSIjnlCTjFzlK7Klb604HLGjsNqU/i5mJLjQ==');
+      element.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/themes/prism-okaidia.min.css');
     }
   }
 
   else {
     localStorage.setItem('theme', 'light');
-    element = document.getElementById("prism-theme")
+    element = document.getElementById("prism-theme");
     if (element) {
-      element.setAttribute('integrity', 'sha512-/mZ1FHPkg6EKcxo0fKXF51ak6Cr2ocgDi5ytaTBjsQZIH/RNs6GF6+oId/vPe3eJB836T36nXwVh/WBl/cWT4w==')
-      element.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/themes/prism.min.css')
+      element.setAttribute('integrity', 'sha512-/mZ1FHPkg6EKcxo0fKXF51ak6Cr2ocgDi5ytaTBjsQZIH/RNs6GF6+oId/vPe3eJB836T36nXwVh/WBl/cWT4w==');
+      element.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/themes/prism.min.css');
     }
   }
 }
