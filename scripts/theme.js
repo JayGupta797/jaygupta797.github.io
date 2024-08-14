6f3ca73ea28acda37bb671e2b13e935a60f65062
Jay Gupta <66860950+JayGupta797@users.noreply.github.com>
2024-08-14 15:53:42 -0700

    Update Themes
    removed integrity attribute from prism, changed dark theme, renamed color-svg.js to color.js

--- scripts/theme.js
+++ scripts/theme.js
@@ -77,14 +77,11 @@ function applyTheme() {
 
     // Toggle Code Blocks
     let prismHref = theme === 'dark' ?
-        'https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/themes/prism-okaidia.min.css' :
+        'https://cdnjs.cloudflare.com/ajax/libs/prism-themes/1.9.0/prism-atom-dark.min.css' :
         'https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/themes/prism.min.css';
 
     let prismElement = document.getElementById("prism-theme");
     if (prismElement) {
-        prismElement.setAttribute('integrity', theme === 'dark' ?
-            'sha512-5HvW0a7ihK3ro2KhwEksDHXgIezsTeZybZDIn8d8Y015Ny+t7QWSIjnlCTjFzlK7Klb604HLGjsNqU/i5mJLjQ==' :
-            'sha512-/mZ1FHPkg6EKcxo0fKXF51ak6Cr2ocgDi5ytaTBjsQZIH/RNs6GF6+oId/vPe3eJB836T36nXwVh/WBl/cWT4w==');
         prismElement.setAttribute('href', prismHref);
     }
 
